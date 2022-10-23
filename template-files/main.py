b27dc7beaf665290d66698480de361169f849e6d
mohammad-nassar10 <mohammad.nassar@ibm.com>
2022-10-23 18:58:43 +0300

    Fix template.
    Signed-off-by: mohammad-nassar10 <mohammad.nassar@ibm.com>

--- template-files/main.py
+++ template-files/main.py
@@ -1,11 +1,11 @@
 from module.config import Config
-from module.s3 import s3_connection, delete_bucket_if_empty, delete_object, get_s3_credentials_from_vault
+from module.s3 import s3_connection, get_s3_credentials_from_vault
 from fybrik_python_logging import init_logger, logger
 
 
 if __name__ == "__main__":
-    init_logger("INFO", "", 'delete-module')
-    logger.info('Delete module initialized')
+    init_logger("INFO", "", 'template-module')
+    logger.info('Template module initialized')
 
     conf = Config("/etc/conf/conf.yaml")
     conf_data = conf.values["data"][0]
@@ -18,10 +18,10 @@ if __name__ == "__main__":
 
     s3_resource = s3_connection(endpoint, access_key, secret_key)
 
-    delete_object(s3_resource, bucket_name, object_key)
-    delete_empty_bucket = False # will be set to get the vaule form configmap when fybrik will have that feature
-    if delete_empty_bucket:
-        delete_bucket_if_empty(s3_resource, bucket_name)
+    logger.info(f"Configuration data: '{conf_data}'")
+
+    logger.info("Successfully done")
+
 
 
 
